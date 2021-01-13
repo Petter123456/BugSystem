0ca8acd60b8b1ccd23f6a850e77a8156807b5267
Petter Fagerlund <petter.fagerlund@gmail.com>
2021-01-13 01:13:06 +0100

    updates dockerfile

--- frontend/Program.cs
+++ frontend/Program.cs
@@ -14,6 +14,8 @@ namespace frontend
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+
+
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
